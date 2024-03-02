70d8070ec5a3e35d9d2b75fb15441d7a2187eb3d
cristiang83 <156969890+cristiang83@users.noreply.github.com>
2024-03-02 00:45:57 +0000

    change my teplate

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { Button, Collapse, Container, Row } from "react-bootstrap";
+
+export default function () {
+  return 8
+    < Container >
+    <Row>
+      <Col>
+        <h1> My own react template</h1>
+        <Button variant="primary">Click</Button>
+      </Col>
+    </Row>
+ </ Container >
+}
